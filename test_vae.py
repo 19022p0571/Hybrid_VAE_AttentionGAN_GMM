d9e22233896683e47d5849cf620ff9b08280d848
19022p0571 <19022p0571@jntugv.edu.in>
2026-07-13 22:25:05 +0530

    Update test_vae.py

--- test_vae.py
+++ test_vae.py
@@ -1,6 +1,6 @@
 import torch
 
-from models.vae import VAE
+from models.vae import VAE, vae_loss
 
 model = VAE()
 
@@ -8,7 +8,13 @@ x = torch.randn(4, 6, 64, 64)
 
 reconstruction, mu, logvar = model(x)
 
-print("Input           :", x.shape)
-print("Reconstruction  :", reconstruction.shape)
-print("Mu              :", mu.shape)
-print("LogVar          :", logvar.shape)
+loss = vae_loss(
+    reconstruction,
+    x,
+    mu,
+    logvar
+)
+
+print("Total Loss :", loss["total_loss"].item())
+print("Reconstruction Loss :", loss["reconstruction_loss"].item())
+print("KL Loss :", loss["kl_loss"].item())
