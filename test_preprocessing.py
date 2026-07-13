4193627d73e52c127afd40ca10c91c99df89e1a0
19022p0571 <19022p0571@jntugv.edu.in>
2026-07-13 21:31:10 +0530

    Create test_preprocessing.py

---
+++ test_preprocessing.py
@@ -0,0 +1,7 @@
+from utils.preprocessing import Sentinel2Preprocessor
+
+safe_path = r""C:\Users\JK\Downloads\S2B_MSIL2A_20211028T051929_N0500_R062_T44RKT_20230107T041610.SAFE""
+
+processor = Sentinel2Preprocessor(safe_path)
+
+processor.run_pipeline()
