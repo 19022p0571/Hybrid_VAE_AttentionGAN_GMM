d79c6a2f778cd8295606eff0445919568f60c3af
19022p0571 <19022p0571@jntugv.edu.in>
2026-07-13 22:04:34 +0530

    Update test_vae.py

--- test_vae.py
+++ test_vae.py
@@ -1,21 +1,14 @@
 import torch
 
-from models.vae import Encoder, Decoder
+from models.vae import VAE
 
-encoder = Encoder()
+model = VAE()
 
-decoder = Decoder()
+x = torch.randn(4, 6, 64, 64)
 
-x = torch.randn(2, 6, 64, 64)
+reconstruction, mu, logvar = model(x)
 
-mu, logvar = encoder(x)
-
-print("Mu:", mu.shape)
-
-print("LogVar:", logvar.shape)
-
-z = torch.randn(2, 100)
-
-output = decoder(z)
-
-print("Output:", output.shape)
+print("Input           :", x.shape)
+print("Reconstruction  :", reconstruction.shape)
+print("Mu              :", mu.shape)
+print("LogVar          :", logvar.shape)
