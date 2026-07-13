3c757982b7d55f05c6b95e7b0276f95f4ce47c41
19022p0571 <19022p0571@jntugv.edu.in>
2026-07-14 00:11:32 +0530

    Update gan_trainer.py

--- trainer/gan_trainer.py
+++ trainer/gan_trainer.py
@@ -33,3 +33,92 @@ class GANTrainer:
             lr=config.LEARNING_RATE,
             betas=(0.5, 0.999)
         )
+
+    def train_epoch(self):
+
+        self.generator.train()
+        self.discriminator.train()
+
+        total_g_loss = 0.0
+        total_d_loss = 0.0
+
+        for real_images in self.train_loader:
+
+            real_images = real_images.to(config.DEVICE)
+
+            batch_size = real_images.size(0)
+
+            real_labels = torch.ones(
+                batch_size,
+                1,
+                device=config.DEVICE
+            )
+
+            fake_labels = torch.zeros(
+                batch_size,
+                1,
+                device=config.DEVICE
+            )
+
+            # -------------------------
+            # Train Discriminator
+            # -------------------------
+
+            self.optimizer_d.zero_grad()
+
+            real_output = self.discriminator(real_images)
+
+            d_loss_real = self.criterion(
+                real_output,
+                real_labels
+            )
+
+            z = torch.randn(
+                batch_size,
+                config.LATENT_DIM,
+                device=config.DEVICE
+            )
+
+            fake_images = self.generator(z)
+
+            fake_output = self.discriminator(
+                fake_images.detach()
+            )
+
+            d_loss_fake = self.criterion(
+                fake_output,
+                fake_labels
+            )
+
+            d_loss = d_loss_real + d_loss_fake
+
+            d_loss.backward()
+
+            self.optimizer_d.step()
+
+            # -------------------------
+            # Train Generator
+            # -------------------------
+
+            self.optimizer_g.zero_grad()
+
+            output = self.discriminator(fake_images)
+
+            g_loss = self.criterion(
+                output,
+                real_labels
+            )
+
+            g_loss.backward()
+
+            self.optimizer_g.step()
+
+            total_d_loss += d_loss.item()
+            total_g_loss += g_loss.item()
+
+        n = len(self.train_loader)
+
+        return {
+            "generator_loss": total_g_loss / n,
+            "discriminator_loss": total_d_loss / n
+        }
